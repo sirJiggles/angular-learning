45b3bfdb5931002d2ca3daad023fb4e3f6830ac6
Gareth Fuller <gareth.fuller@clark.de>
2018-04-10 22:12:18 +0200

    :rewind: revert back to "start" state, took the wrong branch

--- frontendmasters/awesomerAppsWithAngular/src/app/app.module.ts
+++ frontendmasters/awesomerAppsWithAngular/src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppMaterialModule } from './app-material.module';
 
-import { ItemsService, NotificationsService, WidgetsService } from './shared';
+import { ItemsService } from './shared';
 
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import { ItemsListComponent } from './items/items-list/items-list.component';
 import { ItemDetailComponent } from './items/item-detail/item-detail.component';
-import { ItemsSearchComponent } from './items/items-search/items-search.component';
-import { WidgetsComponent } from './widgets/widgets.component';
-import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
-import { WidgetDetailComponent } from './widgets/widget-detail/widget-detail.component';
 import { HomeComponent } from './home/home.component';
-import { NewsletterComponent } from './newsletter/newsletter.component';
-import { ItemComponent } from './items/item/item.component';
 
 @NgModule({
   declarations: [
@@ -27,28 +21,17 @@ import { ItemComponent } from './items/item/item.component';
     HomeComponent,
     ItemsComponent,
     ItemsListComponent,
-    ItemDetailComponent,
-    ItemsSearchComponent,
-    WidgetsComponent,
-    WidgetsListComponent,
-    WidgetDetailComponent,
-    NewsletterComponent,
-    ItemComponent
+    ItemDetailComponent
   ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
     FormsModule,
     HttpModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     AppMaterialModule
   ],
-  providers: [
-    ItemsService,
-    NotificationsService,
-    WidgetsService
-  ],
+  providers: [ItemsService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
