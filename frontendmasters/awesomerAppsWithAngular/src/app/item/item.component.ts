7590151555a915f8cd6313e8bf8165fb11e75f08
Gareth Fuller <gareth.fuller@clark.de>
2018-04-26 23:14:29 +0200

    use route params to get the stream of data form the route!

--- frontendmasters/awesomerAppsWithAngular/src/app/item/item.component.ts
+++ frontendmasters/awesomerAppsWithAngular/src/app/item/item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemsService } from '../shared';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Item } from '../interfaces/item';
 
 @Component({
   selector: 'app-item',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./item.component.css']
 })
 export class ItemComponent implements OnInit {
-
-  constructor() { }
+  item: Item;
+  constructor(
+    private itemsService: ItemsService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.itemsService.load(params.get('id')))
+      .subscribe(item => (this.item = item));
   }
 
+  saveItem(item: Item) {
+    // do something
+  }
+  cancelSavingItem(item: Item) {
+    // do something
+  }
 }
